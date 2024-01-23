c3170a5da5ef769f9153afefc12006973ca48740
hi0ki <ansarimehdi518@gmail.com>
2024-01-23 21:58:36 +0100

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ansa <eel-ansa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 00:00:20 by eel-ansa          #+#    #+#             */
+/*   Updated: 2024/01/23 19:03:47 by eel-ansa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 6
+# endif
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_save(char *save);
+char	*ft_line(char *save);
+char	*ft_read(int fd, char *save);
+int		ft_strlen(char *str);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+int		chr_n(char *str);
+
+#endif
